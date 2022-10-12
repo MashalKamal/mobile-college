4c129bbb6ec6ae30019fec7fe2439f542e58b175
Danelly <danellysantos01@gmail.com>
2022-10-11 20:22:01 -0400

    Initial Commit

---
+++ mobile college/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mobile college
+//
+//  Created by mac14 on 10/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+       super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+
+   }
+    
+}
